d7a8206aad863e9ff90ad1a2d5517ccff588d1e6
alex32mv <alexander.matveev2019@campus-eni.fr>
2021-01-12 11:26:51 +0100

    Encode the persisted data

--- src/com/jmail/controller/persistence/PersistenceAccess.java
+++ src/com/jmail/controller/persistence/PersistenceAccess.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class PersistenceAccess {
 
     private String VALID_ACCOUNTS_LOCATION = System.getProperty("user.home") + File.separator + "validAccounts.ser";
+    private Encoder encoder = new Encoder();
 
     public List<ValidAccount> loadFromPersistence() {
         List<ValidAccount> resultList = new ArrayList<ValidAccount>();
@@ -14,10 +15,25 @@ public class PersistenceAccess {
             final var fileInputSystem = new FileInputStream(VALID_ACCOUNTS_LOCATION);
             final var objectInputStream = new ObjectInputStream(fileInputSystem);
             final List<ValidAccount> persistedList = (List<ValidAccount>) objectInputStream.readObject();
+            decodePasswords(persistedList);
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
         return resultList;
+    }
+
+    private void decodePasswords(List<ValidAccount> persistedList) {
+        for (var validAccount : persistedList) {
+            String originalPassword = validAccount.getPassword();
+            validAccount.setPassword(encoder.decode(originalPassword));
+        }
+    }
+
+    private void encodePasswords(List<ValidAccount> persistedList) {
+        for (var validAccount : persistedList) {
+            String originalPassword = validAccount.getPassword();
+            validAccount.setPassword(encoder.encode(originalPassword));
+        }
     }
 
     public void saveToPersistance(List<ValidAccount> validAccounts) {
@@ -26,6 +42,7 @@ public class PersistenceAccess {
             final var fileOutputStream = new FileOutputStream(file);
             final var objectOutputStream = new ObjectOutputStream(fileOutputStream);
 
+            encodePasswords(validAccounts);
             objectOutputStream.writeObject(validAccounts);
             objectOutputStream.close();
             fileOutputStream.close();
